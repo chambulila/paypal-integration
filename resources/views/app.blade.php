0a57b54c5d9d8abc55e24cb329c0da6b6b9bcea6
Chambulila <kelvinchambulila5@gmail.com>
2023-10-19 15:57:44 +0300

    done authentication module

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -406,6 +406,24 @@ $thispage = request()->segment(1);
                                     Light/Dark Mode
                                 </button>
                             </li>
+                            <li class="flex">
+                                <a class="inline-flex items-center w-full px-2 py-2 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100  hover:text-purple-500 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                                    href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                    <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none"
+                                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        viewBox="0 0 24 24" stroke="currentColor">
+                                        <path
+                                            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1">
+                                        </path>
+                                    </svg>
+                                    <span>Log out</span>
+                                </a>
+                            </li>
                               <li class="flex">
                                   {{-- <a class="inline-flex items-center w-full px-2 py-2 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100  dark:hover:bg-gray-800 dark:hover:text-gray-200"
                                       href="{{ route('logout') }}"
